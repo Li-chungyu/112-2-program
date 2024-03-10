6333de8515b5f2ba609752fb44b2ae9c14455b5d
Li-chungyu <sses100075@gmail.com>
2024-03-10 21:49:51 +0800

    week1 revised

--- oj/week1/hm1.cpp
+++ oj/week1/hm1.cpp
@@ -3,22 +3,36 @@ using namespace std;
 
 class Point{
     public:
-        float x[1024];
-        float y[1024];
+        float p1[1024];
+        float p2[1024];
+};
+
+class Get{
+    public:
+        int N;
+        Point point;
+        void readN(){
+            cin >> N; 
+        }
+        int getN(){
+            return N;
+        }
+        void readP(){
+            for(int i=0;i<getN();i++){
+                cin >> point.p1[i] >> point.p2[i];
+            }
+        }
+        void outp(){
+            for(int i=0;i<getN();i++){
+                cout << "(" << point.p1[i] << ", " << point.p2[i] << ")" << endl;
+            }
+        }
 };
 
 int main(){
-    Point point;
-    int N;
-    int count=0;
-    cin >> N;
-    while(count<N){
-        cin >> point.x[count];
-        cin >> point.y[count];
-        count++;
-    }
-    for(int i=0;i<N;i++){
-        cout << "(" << point.x[i] << ", " << point.y[i] << ")" << endl;
-    }
+    Get g;
+    g.readN();
+    g.readP();
+    g.outp();
     return 0;
 }
